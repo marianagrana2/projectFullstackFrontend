fc01372457a10f63127a1e53e086ed0a15c4c0b3
marianagrana2 <mariana.grana2@gmail.com>
2023-12-17 16:54:46 -0600

    ✨ Search page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,31 @@ import Spinner from "../components/Spinner"
 const Home = () => {
   return (
     <>
+    <section className="heading">
+        <h1>Search</h1>
+        <p>Find your favorite band or artist</p>
+    </section>
+    <section className="form">
+        <form>
+            <div className="form-group">
+                <input
+                type="text"
+                className="form-control"
+                id="searchValue"
+                name='searchValue' 
+                placeholder='Search Artist or Band'
+                >
+                </input>
+            </div>
+            <div className="form-group">
+            <button 
+                type="submit" 
+                className="btn btn-block"
+                >Search
+                </button>
+            </div>
+        </form>
+    </section>
     
     </>
   )
