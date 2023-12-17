741f9aa5d947eb6d67ce29c75cfb81a1fdf6ca4a
marianagrana2 <mariana.grana2@gmail.com>
2023-12-17 17:11:15 -0600

    ✨ login & register base

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,43 @@
+import { FaSignInAlt} from 'react-icons/fa'
 
 const login = () => {
   return (
-    <div>login</div>
+    <>
+    <section className="heading">
+        <h1>
+        <FaSignInAlt/> Login
+        </h1>
+        <p>Please create your account</p>
+    </section>
+    <section className="form">
+        <form>
+            <div className="form-group">
+            <input 
+                type="email"
+                className="form-control"
+                id="email"
+                name='email'
+                placeholder='Your email here'
+                 />
+                 <input 
+                type="password"
+                className="form-control"
+                id="password"
+                name='password'
+                placeholder='Your password here'               
+                 />
+            </div>
+            <div className="form-group">
+                <button 
+                type="submit" 
+                className="btn btn-block"
+                >
+                    Submit
+                </button>
+            </div>
+            </form>
+    </section>
+    </>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,60 @@
+import { FaUser} from 'react-icons/fa'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+    <section className="heading">
+        <h1>
+        <FaUser /> Register
+        </h1>
+        <p>Please create your account</p>
+    </section>
+    <section className="form">
+        <form>
+            <div className="form-group">
+                <input 
+                type="text"
+                className="form-control"
+                id="name"
+                name='name'
+                placeholder='Your name here'
+                
+                 />
+                 <input 
+                type="email"
+                className="form-control"
+                id="email"
+                name='email'
+                placeholder='Your email here'
+                
+                 />
+                 <input 
+                type="password"
+                className="form-control"
+                id="password"
+                name='password'
+                placeholder='Your password here'
+                
+                 />
+                 <input 
+                type="password"
+                className="form-control"
+                id="password2"
+                name='password2'
+                placeholder='Confirm your password'   
+                 />
+            </div>
+            <div className="form-group">
+                <button 
+                type="submit" 
+                className="btn btn-block"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    </section>
+    </>
   )
 }
 
