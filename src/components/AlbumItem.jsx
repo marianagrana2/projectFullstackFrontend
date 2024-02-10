260ff56890193c3d539aa0882c12672a733bfe98
marianagrana2 <mariana.grana2@gmail.com>
2024-02-09 19:19:19 -0600

    ✅ Ya se ve album guardado en pantalla :)

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,19 +1,23 @@
 import { useDispatch } from "react-redux";
 import { deleteAlbum} from "../features/albums/albumSlice";
+import { useSelector } from "react-redux";
 
 const AlbumItem = ({album}) => {
     const dispatch = useDispatch()
+    const albums = useSelector((state) => state.album.albums)
 
     return (
     <div className="album">
-        <div className="col-4 card w-50" key={album._id}>
-            <h5 className="card-title">Album:{album.strAlbum}</h5>
-            <h6 className="card-text">Year:{album.intYearReleased}</h6>
-            {album.strAlbum && 
-            <p>Album: {album.strAlbum}</p>
-            }
-            <button className="close" onClick={()=> dispatch(deleteAlbum(album._id))}> X</button>
-        </div>
+        {albums.map((album)=> (
+             <div className="col-4 card w-50" key={album._id}>
+             <h5 className="card-title">Album:{album.albumName}</h5>
+             <h6 className="card-text">Year:{album.albumYear}</h6>
+             {album.albumName && 
+             <p>Album: {album.albumName}</p>
+             }
+             <button className="close" onClick={()=> dispatch(deleteAlbum(album._id))}> X</button>
+         </div>
+        ))}
     </div>
     )
 }
